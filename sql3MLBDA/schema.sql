71b1b912e81cc29013635ec13bc692541b0d4366
Niko7srb <73240321+Niko7srb@users.noreply.github.com>
2022-09-27 22:18:42 +0200

    Add files via upload

---
+++ sql3MLBDA/schema.sql
@@ -0,0 +1,38 @@
+-- compléter l'entête 
+-- ==================
+
+-- NOM    :
+-- Prénom :
+
+-- NOM    :
+-- Prénom :
+
+-- Groupe :
+-- binome :
+
+-- ================================================
+
+-- nettoyer le compte
+-- ------------------
+--drop type T_un_type force;
+
+
+-- Définition des types de données
+-- -------------------------------
+
+create type T_un_type as object (
+ un_attribut Number
+)
+;
+/
+show errors
+
+
+
+
+
+
+
+-- liste de tous les types créés
+@liste
+
